b47858dbdc4034cd76d428dc3d01f5fcd06023ab
Nellyssc <nellysalama0@gmail.com>
2023-02-18 12:55:01 -0800

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-
-
 /**
- * main - prints "Programming is like building a multilingual
- * puzzle, followed by a newline.
- * Return: Always 0.
+ * main - entry point
+ * Return: always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
 
-	return(0);
-}	
 
