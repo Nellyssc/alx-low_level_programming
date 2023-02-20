209a81e170947aa9ce05570b5b62803f14323ebe
Nellyssc <nellysalama0@gmail.com>
2023-02-20 06:12:33 -0800

    k

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,8 @@
+#include <unistd.h>
+/**main - prints some given text
+ * Return: Always 1.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" -Dora koppar, 2015-10-19\n", 59);
+	return (1);
