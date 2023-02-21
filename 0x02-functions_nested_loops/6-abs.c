8450bee5d9d870d23e8a242ddb86a15628b16498
Nellyssc <nellysalama0@gmail.com>
2023-02-21 13:48:44 -0800

    G

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_abs - computation of absolute value
+ *@h : the integer
+ *Return: absolute value
+ */
+int _abs(int h)
+{
+	if (h >= 0)
+		return (h);
+	else
+		return (-h);
+}
+
