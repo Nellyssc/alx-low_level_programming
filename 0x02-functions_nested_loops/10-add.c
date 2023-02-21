4bb4112b573c8afde7f5b12b56dd9845f0bf11e0
Nellyssc <nellysalama0@gmail.com>
2023-02-21 15:44:34 -0800

    F

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - it should sum the given integers
+ * @n2: digit to be added
+ *@n3: digit to be added
+ * Return: added value.
+ */
+int add(int n3, int n2)
+{
+	return (n3 + n2);
+}
