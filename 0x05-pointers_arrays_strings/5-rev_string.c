d5d5c06079cd727e851279b0ad6aa9305990ea68
Nellyssc <nellysalama0@gmail.com>
2023-02-27 10:43:26 -0800

    LKJ

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * rev_string(char *s) - it will reverse a string
+ * rev_string - it will reverse a string
  * @s: Input string
  * description the function will reverse a given string
  * Return: reversed string
@@ -11,15 +11,15 @@ void rev_string(char *s)
 {
 	char rev = s[0];
 	int counter = 0;
-	int nel;
+	int j;
 
 	while (s[counter] != '\0')
 	counter++;
-	for (nel = 0; nel < counter; nel++)
+	for (j = 0; j < counter; j++)
 	{
 		counter--;
-		rev = s[nel];
-		s[nel] = s[counter];
+		rev = s[j];
+		s[j] = s[counter];
 		s[counter] = rev;
 	}
 }
