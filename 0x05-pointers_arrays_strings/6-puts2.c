8891e405d46f152d973a51b29398dd90c264d81a
Nellyssc <nellysalama0@gmail.com>
2023-02-27 09:31:53 -0800

    j

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts2 - it will print evry character of a string then anew line
+ * beginning with the first one
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int nell = 0;
+	int q = 0;
+	char *h = str;
+
+	int = j;
+
+	while (*h != '\0')
+	{
+		p++;
+		nell++;
+	}
+	q = nel--;
+
+	for (j = 0 ; j <= q ; j++)
+	{
+		if (j % 2 == 0)
+	{
+		_putchar(str[j]);
+	}
+	}
+	_putchar('\n');
+}
