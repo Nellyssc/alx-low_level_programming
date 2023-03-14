03d61160da4ecee3169b832a539cb4243ca61566
Nellyssc <nellysalama0@gmail.com>
2023-03-14 14:29:08 -0700

    Jesus thank you

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,19 +7,19 @@
  * Return: 0
  */
 char *argstostr(int ac, char **av)
+{
+	int i, n, r = 0, 1 = 0;
+
+	char *str;
 
 	if (ac == 0 || av == NULL)
 
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
-
 	{
-
 		for (n = 0; av[i][n]; n++)
-
 			l++;
-
 	}
 	l += ac;
 
