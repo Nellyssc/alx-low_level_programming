02f3f7881378028b7c3b30996591743f04f23499
Nellyssc <nellysalama0@gmail.com>
2023-03-16 12:33:52 -0700

    thank you God

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *malloc_checked - here we dynamically memory allocate using malloc
+ * @b: allocated bytes
+ * Return: pointer to mem allocated
+ */
+
+void *malloc_checked(unsigned int b)
+
+{
+
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+
+		exit(404);
+
+	return (ptr);
+
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+ this  is abou memory allocation using malloc calloc and reloc baasically we will learn and code  and know when to use what exactly
+realloc  initiallizes the values while malloc doesnt
